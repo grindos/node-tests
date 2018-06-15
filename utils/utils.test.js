d62d73fe2c1fd4eb58b83abb662a13184375ab70
grindos <grisha_ya@inbox.ru>
2018-06-15 12:46:37 +0300

    Add several generic tests using Mocha and Expect

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,18 +1,35 @@
+const expect = require('expect')
+
 const utils = require('./utils')
 
 it('should add two numbers', () => {
   let res = utils.add(33, 11)
 
-  if (res !== 44) {
-    throw new Error(`Expected 44, but got ${res}.`)
-  }
+  expect(res).toBe(44).toBeA('number')
 
 });
 
 it('should square the number', () => {
   let res = utils.square(15)
 
-  if (res !== 225) {
-    throw new Error(`Expected 225, but got ${res}`)
-  }
+  expect(res).toBe(225).toBeA('number')
 })
+
+it('should verify first and last names are set', () => {
+  let user = {}
+  utils.setName(user, 'greg yanushkovskiy')
+
+  expect(user).toInclude({firstName: 'greg', lastName: 'yanushkovskiy'})
+})
+
+// it('should expect some values', () => {
+//   // expect(12).toNotBe(11)
+//   // expect({name: 'greg'}).toEqual({name: 'Greg'})
+//   // expect([2,3,4]).toExclude(1)
+//   expect({
+//     name: 'Greg',
+//     age: 24
+//   }).toExclude({
+//     age: 23
+//   })
+// })
