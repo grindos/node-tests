90a15db1f29fd1bc4f479225f1838048e2e51502
grindos <grisha_ya@inbox.ru>
2018-06-15 22:37:27 +0300

    Add tests for express app using supertest module

--- server/server.js
+++ server/server.js
@@ -3,7 +3,29 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Hello world!')
+  res.status(404).send({
+    error: 'Page not found.',
+    name: 'Todo App v1.0'
+  })
+})
+
+app.get('/users', (req, res) => {
+  res.status(200).send([
+    {
+      name: 'Greg',
+      age: 24
+    },
+    {
+      name: 'Dasha',
+      age: 25
+    },
+    {
+      name: 'Morty',
+      age: 0
+    }
+  ])
 })
 
 app.listen(3000)
+
+module.exports.app = app
